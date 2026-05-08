4670909bcd0988fc968fa848779ca053019b87c3
Martin Ivanov <martin.ivanov@adesso.bg>
2026-05-08 10:30:04 +0300

    Fix refactoring plugin

---
+++ lua/plugins/fix_refactoring.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "lewis6991/async.nvim",
+    },
+  },
+  -- Manually ensure async.nvim is present in the spec
+  { "lewis6991/async.nvim" },
+}
